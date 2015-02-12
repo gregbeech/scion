308ddef8dfca76554f0d623477669bdfca68be6f
Greg Beech <greg@blinkbox.com>
2015-02-12 18:40:55 +0000

    A bunch of cleanup

--- lib/scion.rb
+++ lib/scion.rb
@@ -1,3 +1,4 @@
+require "json"
 require "rack"
 
 module Scion
@@ -12,69 +13,95 @@ module Scion
     end
   end
 
+  module Routing
 
-  class RouteNode
-    attr_reader :name, :children
-
-    def initialize(name, children = [])
-      @name = name
-      @children = children
+    def self.is_rejection?(result)
+      result.respond_to?(:any?) && result.any? { |v| v.is_a?(Rejection) }
     end
 
-    def each
-      stack = [self]
-      until stack.empty? do
-        n = stack.pop
-        yield n
-        n.children.reverse.each { |c| stack.push(c) }
+    class Node
+      attr_reader :name, :children
+
+      def initialize(name, *children)
+        @name = name
+        @children = children
+      end
+
+      def each
+        stack = [self]
+        until stack.empty? do
+          n = stack.pop
+          yield n
+          n.children.reverse.each { |c| stack.push(c) } # TODO: better?
+        end
+      end
+
+      def <<(child)
+        @children.push(child)
+        self
+      end
+
+      def call(req, res)
+        raise NotImplementedError.new # to be implemented in derived class
+      end
+
+      def or(other)
+        OneOf.new(self, other)
+      end
+
+      def to_s
+        "#{self.class.name.split('::').last}(#{@name}, children = #{@children.size})"
+      end
+
+      def inspect(depth = 0)
+        s = to_s << "\n"
+        depth += 1
+        @children.each { |c| s << ("  " * depth) << c.inspect(depth) }
+        s
       end
     end
 
-    def <<(child)
-      @children.push(child)
-      self
-    end
+    class OneOf < Node
+      def initialize(*children)
+        super("oneOf", *children)
+      end
 
-    def call(req, res)
-      raise NotImplementedError.new # to be implemented in derived class
-    end
+      def call(req, res)
+        @children.each_with_object([]) do |c, obj|
+          result = c.call(req, res)
+          break result unless Routing.is_rejection?(result)
+          obj.push(*result)
+        end
+      end
 
-    def to_s
-      "#{self.class}(#{@name}, children = #{@children.size})"
-    end
-
-    def or(other)
-      OneOfNode.new(self, other)
-    end
-  end
-
-  class BlockNode < RouteNode
-    def initialize(name, children = [])
-      super(name, children)
-      @block = Proc.new
-    end
-
-    def call(req, res)
-      @block.call(req, res)
-    end
-  end
-
-  class OneOfNode < RouteNode
-    def initialize(*children)
-      super("oneOf", children)
-    end
-
-    def call(req, res)
-      @children.each_with_object([]) do |c, obj|
-        result = c.call(req, res)
-        break result unless result.respond_to?(:any?) && result.any? { |v| v.is_a?(Rejection) }
-        obj.push(result)
+      def or(other)
+        self << other
       end
     end
 
-    def or(other)
-      @children << other
+    class Predicate < Node
+      def initialize(name, *children)
+        super(name, *children)
+        @test = Proc.new
+      end
+
+      def call(req, res)
+        rejection = @test.call(req, res)
+        rejection ? [rejection] : children.first.call(req, res)
+      end
     end
+
+    class Complete < Node
+      def initialize
+        super("complete")
+        @block = Proc.new
+      end
+
+      def call(req, res)
+        @block.call(req, res)
+      end
+    end
+
   end
 
 
@@ -86,7 +113,9 @@ module Scion
 
 
   class Base
+    include Rack::Utils
 
+    # TODO: Move this out into the routing module
     class << self
       attr_reader :routes
 
@@ -95,85 +124,85 @@ module Scion
       end
 
       def path(path)
-        node = BlockNode.new("path:#{path}") do |req, res|
-          if req.path == path
-            node.children.first.call(req, res)
-          else
-            [PathRejection.new]
-          end
-        end
-        node << yield
+        Routing::Predicate.new("path:#{path}", yield) { |req, res| PathRejection.new if req.path != path }
       end
 
-      def get
-        node = BlockNode.new("get") do |req, res|
-          if req.request_method == "GET"
-            node.children.first.call(req, res)
-          else
-            [MethodRejection.new("GET")]
-          end
-        end
-        node << yield
+      def request_method(name)
+        Routing::Predicate.new("method:#{name}", yield) { |req, res| MethodRejection.new(name) if req.request_method != name }
       end
 
-      def post
-        node = BlockNode.new("post") do |req, res|
-          if req.request_method == "POST"
-            node.children.first.call(req, res)
-          else
-            [MethodRejection.new("POST")]
-          end
-        end
-        node << yield
+      def delete(&route)
+        request_method("DELETE", &route)
+      end
+
+      def get(&route)
+        request_method("GET", &route)
+      end
+
+      def post(&route)
+        request_method("POST", &route)
+      end
+
+      def put(&route)
+        request_method("PUT", &route)
       end
 
       def complete(*args)
-        BlockNode.new("complete") do |req, res|
+        # TODO: This properly
+        Routing::Complete.new do |req, res|
+          payload = args[1].to_json
           headers = { 
-            "Content-Length" => args[1].size.to_s,
-            "Content-Type" => "text/plain"
+            "Content-Length" => payload.size.to_s,
+            "Content-Type" => "application/json"
           }
-          [args[0], headers, [args[1]]]
+          [args[0], headers, [payload]]
         end
       end
 
     end
 
-    # ------------------
 
     def call(env)
       dup.call!(env)
     end
 
     def call!(env)
-      # puts self.class.routes
-
       request = Request.new(env)
       response = Response.new
 
+      result = []
       self.class.routes.each do |route|
-        ress = route.call(request, response)
-        p ress
-        return ress
-        # route.each do |node|
-        #   puts node
-        #   p node.call(request, response)
-        # end
+        result = route.call(request, response)
+        return result unless Routing.is_rejection?(result)
       end
 
-      content = "Hello World"
-
-      headers = { 
-        "Content-Length" => content.size.to_s,
-        "Content-Type" => "text/plain"
-      }
-
-      [200, headers, [content]]
+      handle_rejections(result)
     end
 
+    def handle_rejections(rejections)
+      primary = rejections.first
+      case primary
+      when PathRejection then error(404)
+      when MethodRejection then error(405)
+      else error(500)
+      end
+    end
 
+    private
+
+    # TODO: This properly
+    def error(status)
+      payload = { 
+        status: status, 
+        developer_message: Rack::Utils::HTTP_STATUS_CODES[status]
+      }.to_json
+      headers = { 
+        "Content-Length" => payload.size.to_s,
+        "Content-Type" => "application/json"
+      }
+      [status, headers, [payload]]
+    end
 
   end
-
 
 end
--- myapp.rb
+++ myapp.rb
@@ -5,9 +5,9 @@ class MyApp < Scion::Base
   route {
     path("/") {
       get {
-        complete(200, "You got stuff")
+        complete(200, { wow: "You got stuff" })
       }.or post {
-        complete(201, "You created stuff")
+        complete(201, { cool: "You created stuff" })
       }
     }
   }
